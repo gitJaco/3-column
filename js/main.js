4946d83916aafb8336f5a0ddc0819128bb01c789
JacoboDeHoyos <jacopeth88@gmail.com>
2022-11-12 23:08:39 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//Guardar el estado (columna activa)
+let columnaActiva = 1;
+
+//Seleccionar las columnas//
+const columnas = document.querySelectorAll(".columna")
+
+//Escuchar clicks en cada una de ellas//
+columnas.forEach((columna, indice) => {
+    columna.addEventListener("click", function() {
+        cambiarColumna(indice)
+    })
+      
+})
+
+//Cambiar el estado de las columnas//
+function cambiarColumna(indice) {
+    columnas[columnaActiva].classList.remove("activa")
+    columnas[indice].classList.add("activa")
+    columnaActiva = indice
+}
